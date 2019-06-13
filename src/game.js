783bee87c00bb28a99466e9090e4b4ac07eaaa7d
Anna Dontcova <a.dontcova@gmail.com>
2019-06-13 15:13:28 +0200

    Complete Iteration 4

--- src/game.js
+++ src/game.js
@@ -1,7 +1,35 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    for (var x = 0; x <= width; x += width / 10) {
+      for (var y = 0; y <= height; y += height / 10) {
+        stroke(0);
+        strokeWeight(1);
+        line(x, 0, x, height);
+        line(0, y, width, y);
+      }
+    }
   }
+  //image(imgDown, player1.row, player1.col, 100, 100);}
+  
+
 }
+
+  class Player {
+    constructor (row, col) {
+      this.row = row; 
+      this.col = col;
+    }
+      moveUp() {
+      this.col -= 100 ;
+      }
+      moveDown() {
+        this.col += 100;
+      }
+      moveLeft() {
+        this.row -= 100;
+      }
+      moveRight() {
+      this.row += 100;
+      }
+  
+    }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,54 @@
 const game = new Game();
+let imgDown;
+let imgUp;
+let imgRight;
+let imgLeft;
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  imgDown = loadImage("../assets/character-down.png");
+  imgUp = loadImage("../assets/character-up.png");
+  imgRight = loadImage("../assets/character-right.png");
+  imgLeft = loadImage("../assets/character-left.png");
 }
+
+const player1 = new Player(0, 0);
 
 function draw() {
   game.drawGrid();
+  
+
+  
+  //image(imgDown, player1.row, player1.col, 100, 100);
+  // image(imgUp, player1.row, player1.col, 100, 100);
+  // image(imgRight, player1.row, player1.col, 100, 100);
+  // image(imgLeft, player1.row, player1.col, 100, 100);
+
 }
+
+
+function keyPressed() {
+
+  if (keyCode === 38) {
+    clear();
+    player1.moveUp();
+    image(imgUp, player1.row, player1.col, 100, 100);
+  } else if (keyCode === 40) {
+    clear();
+    player1.moveDown();
+    image(imgDown, player1.row, player1.col, 100, 100);
+  } else if (keyCode === 37) {
+    clear();
+    player1.moveLeft();
+    image(imgLeft, player1.row, player1.col, 100, 100);
+  } else if (keyCode === 39) {
+    clear();
+    player1.moveRight();
+    image(imgRight, player1.row, player1.col, 100, 100);
+  }
+}
+
+
+
+
