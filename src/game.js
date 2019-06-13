f8f5f48e8dbbe18c1aba60b37ff93cf96e4aabda
Adri Gispen <aegispen@gmail.com>
2019-06-13 15:18:29 +0200

    first version working

--- src/game.js
+++ src/game.js
@@ -1,7 +1,57 @@
 class Game {
-  drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+  constructor() {
+    this.colorMatrix = [[],[],[],[],[],[],[],[],[],[]];
   }
+
+  drawGrid() {
+    let offset = SQUARE_SIDE;
+    for (var i=0; i<11; i++) {
+      line(0, i*offset, offset*10, i*offset);
+      line(i*offset, 0, i*offset, offset*10);
+    }
+  }
+
+  updateBoard(player) {
+    this.colorMatrix[player.row][player.col] = player.color;
+  }
+
+  drawColorMatrix() {
+    for (var i=0; i<10;i++) {
+      for (var j=0; j<10; j++) {
+        if (this.colorMatrix[i][j]) {
+          fill(this.colorMatrix[i][j]);
+          rect(SQUARE_SIDE*j, SQUARE_SIDE*i, SQUARE_SIDE, SQUARE_SIDE);
+        }
+      }
+    }
+  }
+}
+
+class Player {
+  constructor(row, col) {
+    this.col = col;
+    this.row = row;
+
+  }
+
+  moveDown() {
+    this.row < 9 ? this.row++ : console.log('Cannot move down');
+  }
+
+  moveRight() {
+    this.col < 9 ? this.col++ : console.log('Cannot move right');
+  }
+
+  moveUp() {
+    this.row > 0 ? this.row-- : console.log('Cannot move up');
+  }
+
+  moveLeft() {
+    this.col > 0 ? this.col-- : console.log('Cannot move left');
+  }
+
+  drawPlayer() {
+    image(this.image, SQUARE_SIDE*this.col, SQUARE_SIDE*this.row, SQUARE_SIDE, SQUARE_SIDE);
+  }
+
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,86 @@
 const game = new Game();
+const player1 = new Player(0,0);
+const player2 = new Player(9,9);
+
+
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  let imgUp = loadImage("../assets/character-up.png"); 
+  // let imgRight = loadImage("../assets/character-right.png");
+  // let imgLeft = loadImage("../assets/character-left.png");
+  let imgDown = loadImage("../assets/character-down.png");
+  let color1 = color('darkblue');
+  let color2 = color('orange');
+  player1.image = imgDown;
+  player1.color = color1;
+  player2.image = imgUp;
+  player2.color = color2;
+
 }
 
 function draw() {
   game.drawGrid();
+  game.drawColorMatrix();
+  player1.drawPlayer();
+  game.updateBoard(player1);
+  player2.drawPlayer();
+  game.updateBoard(player2);
+  
 }
+
+function keyPressed() {
+  clear();
+  console.log("press");
+  console.log(keyCode);
+  switch(keyCode) {
+    case DOWN_ARROW:
+      player1.moveDown();
+      break;
+    case RIGHT_ARROW:
+      player1.moveRight();
+      break;
+    case LEFT_ARROW:
+      player1.moveLeft();
+      break;
+    case UP_ARROW:
+      player1.moveUp();
+      break;
+    case 87:
+      player2.moveUp();
+      break;
+    case 83: 
+      player2.moveDown();
+      break;
+    case 65:
+      player2.moveLeft();
+      break;
+    case 68:
+      player2.moveRight();
+      break;
+  }
+
+
+  // if (keyCode === ) {
+  //   player1.moveDown();
+  // } else if (keyCode === RIGHT_ARROW) {
+  //   player1.moveRight();
+  // } else if (keyCode === UP_ARROW) {
+  //   player1.moveUp();
+  // } else if (keyCode === LEFT_ARROW) {
+  //   player1.moveLeft();
+  // } 
+}
+// Increase by 1 the value of player.row
+// player.moveDown() // Increase by 1 the value of player.row
+// player.moveDown() // Increase by 1 the value of player.row
+// player.moveDown()
+// player.moveDown() // Increase by 1 the value of player.row
+// player.moveDown()
+// player.moveDown() // Increase by 1 the value of player.row
+// player.moveDown()
+// player.moveDown() // Increase by 1 the value of player.row
+// player.moveDown()
+// player.moveRight() // Increase by 1 the value of player.col
+// console.log(player.col, player.row) // => 1,2
